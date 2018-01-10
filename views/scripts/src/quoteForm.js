7ab452c84fd9bd36e3f691ec2f7013221c09bd84
Lewis Booth <lewis@lewisbooth.co.uk>
2018-01-10 10:23:06 +0000

    Removed responsive form to test

--- views/scripts/src/quoteForm.js
+++ views/scripts/src/quoteForm.js
@@ -135,21 +135,21 @@ function getPrice() {
 
 ////////////////////////////////////
 
-// Move form to top of page on mobile screens
-function moveForm() {
-  if (window.innerWidth < 1024) {
-    topSection.insertBefore(quoteForm, mobileLocation);
-  } else {
-    aside.insertBefore(quoteForm, callToBook);
-  }
-}
+// // Move form to top of page on mobile screens
+// function moveForm() {
+//   if (window.innerWidth < 1024) {
+//     topSection.insertBefore(quoteForm, mobileLocation);
+//   } else {
+//     aside.insertBefore(quoteForm, callToBook);
+//   }
+// }
 
-moveForm();
+// moveForm();
 
-window.addEventListener("resize", function () {
-  moveForm();
-});
+// window.addEventListener("resize", function () {
+//   moveForm();
+// });
 
-function insertAfter(el, referenceNode) {
-  referenceNode.parentNode.insertBefore(el, referenceNode.nextSibling);
-}
+// function insertAfter(el, referenceNode) {
+//   referenceNode.parentNode.insertBefore(el, referenceNode.nextSibling);
+// }
