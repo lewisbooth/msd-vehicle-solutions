20a8ce15c321b46fdb1f57ed79dd44584c01eaec
Lewis Booth <lewis@lewisbooth.co.uk>
2018-02-14 16:22:26 +0000

    Instagram works, theme changed back to blue

--- views/scripts/src/instafeed.js
+++ views/scripts/src/instafeed.js
@@ -5,7 +5,7 @@ var feed = new Instafeed({
   accessToken: "4608838305.fd68254.4931207292a54cdeba2ef44382fcfda8",
   resolution: "low_resolution",
   sortBy: "most-recent",
-  limit: "10",
+  limit: "20",
   template:
     "<a href={{link}} target='_blank' style='background-image: url({{image}}); background-size: cover; background-position: center;'/>"
 });
