ea475db0853dcd313729c4a39cffbbbd1c11ccb7
Lewis Booth <lewis@lewisbooth.co.uk>
2018-02-15 17:18:40 +0000

    Backup/download from S3 working

--- helpers/S3.js
+++ helpers/S3.js
@@ -1,48 +1,83 @@
 const fs = require("fs");
+const moment = require("moment");
+const readline = require('readline');
 const AWS = require("aws-sdk");
-
 AWS.config.loadFromPath(__dirname + "/../variables.aws.json");
-var s3 = new AWS.S3();
+const s3 = new AWS.S3();
 
 exports.upload = (Bucket, Key) => {
+  console.log(`Uploading ${Key} to ${Bucket}`);
   const Body = fs.readFileSync(Key);
   const options = { Body, Bucket, Key };
   s3.upload(options, (err, data) => {
     if (err) {
+      console.log("Error uploading to S3");
       console.log(err);
     } else {
-      console.log(`Successfully uploaded ${Key} to S3 bucket ${Bucket}`);
+      console.log("Upload successful");
       console.log(data.Location);
     }
   });
 };
 
-exports.downloadLatest = (Bucket, saveFolder) => {
-  s3.listObjectsV2({ Bucket }, (err, data) => {
-    if (data.Contents.KeyCount === 0) {
-      console.log("No items found in bucket: " + Bucket)
-      return null;
-    }
-    const Key = data.Contents[data.KeyCount - 1].Key
-    console.log("Downloading " + Key)
+exports.download = (Bucket, Key, saveFolder) => {
+  return new Promise(resolve => {
     const options = { Bucket, Key }
+    const filename = Key.split("/").slice(-1).pop()
     if (!fs.existsSync(saveFolder)) {
       fs.mkdirSync(saveFolder)
     }
-    const writeStream = fs.createWriteStream(`${saveFolder}/latestBackup.tgz`)
-    const fileStream = s3.getObject(options, (err => {
+    const saveFileLocation = `${saveFolder}/${filename}`
+    console.log(filename)
+    const file = fs.createWriteStream(saveFileLocation)
+    const fileStream = s3.getObject(options).createReadStream().pipe(file)
+    file.on('error', () => {
+      console.log("Error downloading file to " + saveFileLocation)
+      resolve(false)
+    })
+    file.on('close', () => {
+      console.log("Successfully downloaded file to " + saveFileLocation)
+      resolve(saveFileLocation)
+    })
+  })
+};
+
+// Presents the user with a list of the latest entries
+// User selects which file to download
+exports.downloadIndex = (Bucket, saveFolder) => {
+  console.log("Looking for entries in " + Bucket)
+  return new Promise(resolve => {
+    s3.listObjectsV2({ Bucket }, async (err, data) => {
+      // Handle errors
       if (err) {
-        console.log("Error downloading file")
         console.log(err)
-        return false
+        return null
+      } else if (data.Contents.KeyCount === 0) {
+        console.log("No items found in bucket: " + Bucket)
+        return null
       }
-    })).createReadStream().pipe(writeStream)
-    console.log("Successfully downloaded file")
-    return true
+      // Sort bucket entries by date
+      const files = data.Contents.sort((a, b) => {
+        return a.LastModified - b.LastModified
+      })
+      // Console log all the entries with a selection index
+      files.forEach((file, i) => {
+        const time = moment(file.LastModified).fromNow()
+        console.log(`[${i}] ${moment(file.LastModified)} (${time})`)
+      })
+      // Prompt user to select which backup they want
+      const selectedFile = await userSelect(files);
+      // Get the key for the user-selected file
+      const Key = files[Object.keys(files)[selectedFile]].Key
+      // Download the file
+      const downloadBackup = await this.download(Bucket, Key, saveFolder)
+      resolve(downloadBackup)
+    })
   })
 }
 
 exports.cleanBucket = (Bucket, limit) => {
+  console.log(`Limiting bucket ${Bucket} to ${limit} items`)
   const options = { Bucket }
   s3.listObjectsV2(options, (err, data) => {
     if (err) {
@@ -60,9 +95,26 @@ exports.cleanBucket = (Bucket, limit) => {
         Bucket,
         Delete: { Objects: outdatedItems }
       }
-      console.log(outdatedItems)
       s3.deleteObjects(deleteOptions, (err, data) => {
       })
     }
   })
 };
+
+function userSelect(files) {
+  return new Promise(resolve => {
+    const input = readline.createInterface(process.stdin, process.stdout, null)
+    input.question("Select a backup entry: ", answer => {
+      answer = parseInt(answer)
+      if (answer >= 0 && answer <= Object.keys(files).length - 1) {
+        selectedFile = answer
+        resolve(selectedFile)
+      } else {
+        console.log("Please select a valid file")
+        userSelect(files)
+      }
+      input.close();
+      process.stdin.destroy();
+    })
+  })
+}
--- helpers/mongo.js
+++ helpers/mongo.js
@@ -1,12 +1,40 @@
 require("dotenv").config({ path: "variables.env" });
-const backup = require("mongodb-backup");
-const restore = require("mongodb-restore");
+const mongoBackup = require("mongodb-backup");
+const mongoRestore = require("mongodb-restore");
 const hasher = require("folder-hash");
 const tar = require("tar");
 const fs = require("fs");
+const S3 = require("./S3");
 
-exports.backup = backupFolder => {
-  let timestamp = new Date()
+exports.restore = async () => {
+  // Downloads most recent S3 backup
+  const downloadFile = await S3.downloadIndex(process.env.S3_BACKUP_BUCKET_NAME, './mongodb/temp')
+  console.log(downloadFile)
+  if (downloadFile === false) process.exit()
+
+
+  // mongoRestore({
+  //   uri: process.env.DATABASE,
+  //   root: "mongodb/backup",
+  //   callback: err => {
+  //     console.log("Error restoring database")
+  //     if (err) console.log(err);
+  //     else {
+  //       console.log("Successfully restored database")
+  //     }
+  //   }
+  // });
+};
+
+exports.backup = () => {
+  console.log("Backing up files");
+  // Create backup folder if it doesn't already exist
+  if (!fs.existsSync("mongodb")) {
+    fs.mkdirSync("mongodb")
+  }
+  // Format timestamp to weekday-month-date-year-hour-min-sec
+  // E.g thu-feb-15-2018-10-11-01 
+  const timestamp = new Date()
     .toString()
     .toLowerCase()
     .split(" ")
@@ -14,27 +42,27 @@ exports.backup = backupFolder => {
     .join("-")
     .split(":")
     .join("-");
-  backup({
+  // Dump database into local backup folder
+  mongoBackup({
     uri: process.env.DATABASE,
-    root: backupFolder,
+    root: "mongodb/backup",
     callback: err => {
       if (err) console.log(err);
       else {
-        console.log("Successfully backed up database")
+        console.log("Successfully backed up database to mongodb/backup")
       }
     }
   });
-};
-
-exports.restore = restoreFolder => {
-  restore({
-    uri: process.env.DATABASE,
-    root: restoreFolder,
-    callback: err => {
-      if (err) console.log(err);
-      else {
-        console.log("Successfully restored database")
-      }
-    }
-  });
-};
+  // Tarball the database dump and vehicle imagery folder
+  tar.c({ file: `mongodb/${timestamp}.tgz` },
+    ["./public/images/vehicles", "./mongodb/backup"]
+  ).then(_ => {
+    // Upload the tarball to S3
+    S3.upload(
+      process.env.S3_BACKUP_BUCKET_NAME,
+      `mongodb/${timestamp}.tgz`
+    );
+  })
+  // Restrict S3 bucket to 30 entries
+  S3.cleanBucket(process.env.S3_BACKUP_BUCKET_NAME, 30)
+}
