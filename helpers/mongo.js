577836577752fcebe00250e3b387ea6d7b787b37
Lewis Booth <lewis@lewisbooth.co.uk>
2018-02-16 15:55:00 +0000

    Typo in restore script

--- helpers/mongo.js
+++ helpers/mongo.js
@@ -24,7 +24,7 @@ exports.restore = async () => {
     if (fs.existsSync("mongodb/temp/mongodb")) {
       mongoRestore({
         uri: process.env.DATABASE,
-        root: "mongodb/temp/mongodb",
+        root: "mongodb/temp/mongodb/backup",
         callback: err => {
           if (err) {
             console.log("Error restoring database")
